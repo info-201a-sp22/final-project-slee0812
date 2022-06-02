cd25e450e6da03717d9f07cabbed7b1d27d5a2db
BrianP99 <myunggeunp@gmail.com>
2022-06-01 22:19:29 -0700

    With Feedback

--- server.R
+++ server.R
@@ -17,7 +17,7 @@ server <- function(input, output) {
     # bar_chart
     bar_chart <- ggplot(data = wins_df) +
       geom_col(mapping = aes(x = reorder(country, +winning_perc), y = winning_perc, fill = country)) +
-      labs(title = "Winning Percentages by Countries in FIFA World Cup (1930-2018)", x = "Countries", y = "Winning Percentages (%)", fill = "Percentages (%)") +
+      labs(title = "Winning Percentages by Countries in FIFA World Cup (1930-2018)", x = "Countries", y = "Winning Percentages (%)", fill = "Country") +
       theme(text = element_text(size = 10, face = "bold"), plot.title = element_text(size = 20, face = "bold")) +
       coord_flip()
 
@@ -35,7 +35,7 @@ server <- function(input, output) {
   output$correalation_plot <- renderPlotly({
     GSplot <- ggplot(data = WorldCup) +
       geom_point(aes(x = MatchesPlayed, y = GoalsScored, colour = MatchesPlayed)) +
-      labs(title = "Correlation between MatchesPlayed and GoalsScored") + 
+      labs(title = "Correlation between MatchesPlayed and GoalsScored") +
       theme(plot.title = element_text(size = 20, face = "bold"))
 
     QTplot <- ggplot(data = WorldCup) +
@@ -57,10 +57,10 @@ server <- function(input, output) {
       return(Atplot)
     }
   })
-  
+
   output$description_2 <- renderText({
     value_2 <- paste("This chart studies the correlation between the Matches played in a year and the total scores. Intuitively speaking, more matches will lead to more goals but their relation might not be perfectly linear, so we include this chart to explore the authentic correlation between these two variables.")
-    
+
     return(value_2)
   })
 
@@ -73,7 +73,7 @@ server <- function(input, output) {
       geom_line(aes(x = Year, y = Home_goals, colour = "Home scores")) +
       geom_line(aes(x = Year, y = Away_goals, colour = "Away scores")) +
       labs(title = "Comparsion between Home Scores and Away Scores") +
-      theme(plot.title = element_text(size = 20, face = "bold")) +
+      theme(plot.title = element_text(size = 20, face = "bold"), axis.text.x = element_text(angle = 90)) +
       print(labs(y = "Goals", x = "Year")) +
       scale_x_continuous(breaks = seq(1930, 2018, by = 4))
 
@@ -81,9 +81,9 @@ server <- function(input, output) {
   })
 
 
-output$description_3 <- renderText({
-  value_3 <- paste("The purpose of this chart is to compare the number of home team goals and away team goals over the history of FIFA World Cup. To do so, our team used a line chart because it is easy to track the number of total goals for each World Cup held in every 4 years and investigate whether home teams have an advantage over away teams. We grouped up each World Cup by its year and summed up total home team goals and away team goals, respectively. In result, we found that the most goals that home teams scored were in 1954 and the most goals that away teams scored were in 2014, which was the most recent World Cup in Russia. Moreover, this chart displays that the home teams scored more than the away teams, but this phenomenon no longer exists in recent years, concluding that home-field advantage does not affect the game result.")
-  
-  return(value_3)
-})
+  output$description_3 <- renderText({
+    value_3 <- paste("The purpose of this chart is to compare the number of home team goals and away team goals over the history of FIFA World Cup. To do so, our team used a line chart because it is easy to track the number of total goals for each World Cup held in every 4 years and investigate whether home teams have an advantage over away teams. We grouped up each World Cup by its year and summed up total home team goals and away team goals, respectively. In result, we found that the most goals that home teams scored were in 1954 and the most goals that away teams scored were in 2014, which was the most recent World Cup in Russia. Moreover, this chart displays that the home teams scored more than the away teams, but this phenomenon no longer exists in recent years, concluding that home-field advantage does not affect the game result.")
+
+    return(value_3)
+  })
 }
